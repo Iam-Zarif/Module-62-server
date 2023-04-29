6e45f90542872b98dd6da743ebb6ee4347a1d637
Zarif <zarifraja19@gmail.com>
2023-04-30 00:40:18 +0600

    added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5020;
-const catagories = require('./Data/catagories.json')
+const port = process.env.PORT || 5022;
+const catagories = require("./Data/catagories.json");
+const news = require("./Data/news.json");
 
 var cors = require("cors");
 app.use(cors());
@@ -9,9 +10,26 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get('/catagories' , (req, res) =>{
-    res.send(catagories);  
-})
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find(n => n._id === id)
+});
+app.get("/catagories", (req, res) => {
+  res.send(catagories);
+});
+app.get("catagories/:id", (req, res)=>{
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if(id ===0){
+    res.send(news);
+  }
+  else {const categoryNews = news.filter(n => parseInt(n.category_id) ===id);
+  res.send(categoryNews)}
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
