4d5b03c20efb4a7d38c36df0f8e84225e67ca5c2
Zarif <zarifraja19@gmail.com>
2023-04-30 21:17:22 +0600

    added

--- index.js
+++ index.js
@@ -8,26 +8,27 @@ var cors = require("cors");
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  res.send("Dragon is running");
+}); 
 app.get("/news", (req, res) => {
   res.send(news);
 });
 app.get("/news/:id", (req, res) => {
   const id = req.params.id;
   console.log(id);
-  const selectedNews = news.find(n => n._id === id)
+  const selectedNews = news.find(n => n._id == id);
+  res.send(selectedNews);
 });
 app.get("/catagories", (req, res) => {
   res.send(catagories);
 });
-app.get("catagories/:id", (req, res)=>{
+app.get("/catagories/:id", (req, res)=>{
   const id = parseInt(req.params.id);
   console.log(id);
-  if(id ===0){
+  if(id ==0){
     res.send(news);
   }
-  else {const categoryNews = news.filter(n => parseInt(n.category_id) ===id);
+  else {const categoryNews = news.filter(n => parseInt(n.category_id) ==id);
   res.send(categoryNews)}
 });
 
